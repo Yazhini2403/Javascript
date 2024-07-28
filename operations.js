32dc1537923d3331a4767a56fa3d7189e271d7fe
Yazhini2403 <727722euec209@skcet.ac.in>
2024-07-28 16:25:16 +0530

    hi

---
+++ operations.js
@@ -0,0 +1,51 @@
+console.log("Arithnetic Operations: ")
+var a=23;
+var b=24;
+console.log("Addition ans =",a+b);
+console.log("Subtraction ans =",a-b);
+console.log("Multiplication ans =",a*b);
+console.log("Division ans =",a/b);
+console.log("Modulus ans =",a%b);
+
+
+console.log("Conditional Operations: ")
+var c=10;
+var d=20;
+console.log("Greater than ans =",a>b);
+console.log("Less than` ans =",a<b);
+console.log("Greater than or equal to ans =",a>=b);
+console.log("Less than or equal to ans =",a<=b);
+console.log("Equal to(==) ans =",a==b);
+console.log("Equal to(===) ans =",a===b);
+console.log("Equal to(!=) ans =",a!=b);
+console.log("Equal to(!==) ans =",a!==b);
+
+
+console.log("Logical Operationss: ")
+var e=2;
+var f=24;
+console.log("Logical And ans =",(a>b)&&(b<a));
+console.log("Logical Or ans =",(a>b)||(b<a));
+console.log("Logical Not ans = ",!(a>b));
+
+
+console.log("Ternary Operationss: ")
+var e=2;
+var f=24;
+console.log("Ternary operator ans =",(a>b)&&(b<a)?true:false);
+
+
+console.log("Bitwise Operationss: ")
+var g=2;
+var h=4;
+console.log("Bitwise Logical And ans =",g&h);
+console.log("Bitwise Logical Or ans =",g|h);
+console.log("Bitwise Logical Not ans = ",~h);
+console.log("Bitwise Logical Zero fill left shift ans = ",g<<h);
+console.log("Bitwise Logical Signed right shift ans = ",g>>h);
+
+
+console.log("Unary Operations: ")
+var i=47;
+console.log("Unary Addition ans =",i++);
+console.log("Unary Subtraction ans =",i--);
